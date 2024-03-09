6ecd5a294c5bdf8edcb437207f462e540234ee72
Andrey <tanzor87@mail.ru>
2024-03-09 13:10:39 +0700

    add OfferClient

--- Seminar_02/java/SuperMarketApp/src/App.java
+++ Seminar_02/java/SuperMarketApp/src/App.java
@@ -17,7 +17,5 @@ public class App {
 
         magnit.update();
 
-
-
     }
 }
--- Seminar_02/java/SuperMarketApp/src/Classes/OfferClient.java
+++ Seminar_02/java/SuperMarketApp/src/Classes/OfferClient.java
@@ -1,2 +1,49 @@
-package Classes;public class OfferClient {
+package Classes;
+
+import Interface.iActorBehaviour;
+
+public class OfferClient extends Actor implements iActorBehaviour {
+    private String nameOffer;
+    private int id;
+    static int numbersClientOffer;
+
+    public OfferClient(String name, String nameOffer, int id) {
+        super(name);
+        this.nameOffer = nameOffer;
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return super.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        super.name = name;
+
+    }
+
+    public boolean isTakeOrder() {
+        return super.isTakeOrder;
+    }
+
+    public boolean isMakeOrder() {
+        return super.isMakeOrder;
+    }
+
+    @Override
+    public void setTakeOrder(boolean value) {
+
+    }
+
+    @Override
+    public void setMakeOrder(boolean value) {
+
+    }
+
+    @Override
+    public Actor getActor() {
+        return null;
+    }
 }
