f437cd56cd2436fbe4d0c0d99fee080227ebd00c
Andrey <tanzor87@mail.ru>
2024-03-10 01:06:36 +0700

    add javadocs to iReturnOrder

--- Seminar_02/java/SuperMarketApp/src/Interface/iReturnOrder.java
+++ Seminar_02/java/SuperMarketApp/src/Interface/iReturnOrder.java
@@ -1,8 +1,23 @@
 package Interface;
 
+/**
+ * Интерфейс возврата товара покупателем в магазин
+ */
 public interface iReturnOrder {
+    /**
+     * Метод постановки покупателя в очередь для возврата товара
+     * @param actor покупатель возвращающий товар
+     */
     public void takeInQueue(iActorBehaviour actor);
+
+    /**
+     * Метод выхода покупателя из очереди
+     */
     public void releaseFromQueue();
+
+    /**
+     * метод возврата товара покупателем в магазин
+     */
     public void returnOrder();
 
 }
