49d55934d96dbf90b603f0b91300c6dc0b09d554
Andrey <tanzor87@mail.ru>
2024-03-14 00:24:21 +0700

    sem 4_2

---
+++ Seminar_03/java/StudentApp/src/Services/EmployeeSrvice.java
@@ -0,0 +1,22 @@
+package Services;
+
+import Domen.Employee;
+import java.util.List;
+
+public class EmployeeSrvice implements IPersonService<Employee> {
+    private int count;
+    private List<Employee> employees;
+
+    @Override
+    public List<Employee> getAll() {
+        return employees;
+    }
+
+    @Override
+    public void create(String name, int age) {
+        Employee employee = new Employee(name, age, "basis");
+        count++;
+        employees.add(employee);
+
+    }
+}
--- Seminar_03/java/StudentApp/src/Services/StudentService.java
+++ Seminar_03/java/StudentApp/src/Services/StudentService.java
@@ -1,4 +1,28 @@
 package Services;
 
-public class StudentService {
+import Domen.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentService implements IPersonService<Student>{
+    private int count;
+    private List<Student> students;
+
+    public StudentService() {
+        students = new ArrayList<>();
+    }
+
+    @Override
+    public List<Student> getAll() {
+        return students;
+    }
+
+    @Override
+    public void create(String name, int age) {
+        Student student = new Student(name, age);
+        count++;
+        students.add(student);
+
+    }
 }
