236ef22298eb78dbc8c314606df8f983761aaab5
Andrey <rsdak@ksc.krasn.ru>
2024-03-16 18:11:51 +0700

    seminar 5 start 2

---
+++ Seminar_03/java/StudentApp/src/Domen/Student.java
@@ -0,0 +1,46 @@
+package Domen;
+
+public class Student extends Person implements Comparable<Student> {
+    private int id;
+    private static int generalId;
+
+    public Student(String name, int age) {
+        super(name, age);
+        this.id = generalId;
+        generalId++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id = " + id + "," +
+                " Name: " + super.getName() + "," +
+                " Age: " + super.getAge() +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+//        System.out.println(super.getName() + " " + o.getName());
+        if (o.getAge() == super.getAge()){
+//            return 0;
+            if (id == o.id) {
+                return 0;
+            }
+            if (id > o.id) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+        if (o.getAge() > super.getAge()){
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
